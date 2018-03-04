75aa96e59aaa8997468d4936d377f50d0b15e459
David Haynes <dahaynes@umn.edu>
2018-03-03 23:12:33 -0600

    fixed syntax errors

--- SciDB_analysis.py
+++ SciDB_analysis.py
@@ -18,7 +18,7 @@ def datasetsprep():
     shapefiles = ["regions.shp","states.shp","counties.shp","tracts.shp"]
 
     arrayTables =  [ "%s_%s" % (array, chunk) for array in array_names for chunk in chunk_sizes ]
-    rasterPaths =  [ raster_path for raster_path in raster_path for chunk in chunk_sizes ]
+    rasterPaths =  [ raster_path for raster_path in raster_paths for chunk in chunk_sizes ]
 
     datasetRuns = [ OrderedDict([("shapepath", s),("array_table", a), ("raster_path", r)] ) if "nlcd" in r else OrderedDict([("shape_path", "%s_proj.shp" % s[:-4]),("array_table", a), ("raster_path", r)] ) for a,r in zip(arrayTables,rasterPaths) for s in shapefiles ]
 
@@ -48,6 +48,7 @@ if __name__ == '__main__':
     analytic = 0
     filePath = ''
     timings = OrderedDict()
+    rasterStatsCSV = ''
 
     datasets = datasetprep()
     for d in datasets:
@@ -62,20 +63,21 @@ if __name__ == '__main__':
             stopRasterization = timeit.default_timer()
             
             sdb_statements = Statements(sdb)
+            
             theAttribute = 'id:%s' % (datapackage[0]) #
-
             sdb_statements.CreateLoadArray('boundary', theAttribute , 2)
             sdb_statements.LoadOneDimensionalArray(-1, 'boundary', theAttribute, 1, 'p_zones.scidb')
             #Load operator -1 in parallel
             numDimensions = raster.CreateMask(datapackage[0], 'mask')
-            stopLoad = timeit.default_timer()
-            raster.GlobalJoin_SummaryStats(raster.SciDBArrayName, 'boundary', 'mask', raster.tlY, raster.tlX, raster.lrY, raster.lrX, numDimensions, args.band, args.csv)
-            stopSummaryStats = timeit.default_timer()
+            redimension_time, summaryStatTime = raster.GlobalJoin_SummaryStats(raster.SciDBArrayName, 'boundary', 'mask', raster.tlY, raster.tlX, raster.lrY, raster.lrX, numDimensions, 1, rasterStatsCSV)
+            
 
             analytic += 1
 
             timings[analytic] = OrderedDict( [("connectionInfo", "XSEDE"), ("run", r), ("SciDB_Executors", raster.SciDBInstances) ), ("array_table", datasets[d]["array_table"]), ("boundary_table", datasets[d]["shape_path"]), \
-                                ("full_time", stopSummaryStats-start), ("join_time", stopSummaryStats-stopLoad), ("load_time", stopLoad - stopRasterization), ("rasterize_time", stopRasterization-stopPrep), ])
+                                ("full_time", stopSummaryStats-start), ("join_time", summaryStatTime), ("redimension_time", redimension_time), ("rasterize_time", stopRasterization-stopPrep), ])
+            sdb.query("remove(mask)")
+            sdb.query("remove(boundary)")
 
     if filePath: WriteFile(filePath, timings)
     print("Finished")
