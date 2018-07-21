2bb53b3e1a331176250d9af198a921cdf83b91aa
David Haynes <dahaynes@umn.edu>
2018-07-21 00:47:39 -0500

    updated for new loading

--- RasterLoader.py
+++ RasterLoader.py
@@ -1,11 +1,11 @@
-from SciDBParallel import RasterLoader, ParallelLoad
+from SciDBParallel import RasterLoader, ParallelLoad, ParallelLoadByChunk
 from scidb import iquery, Statements
 
 
 if __name__ == '__main__':
         datasets = {
-                    #"glc": {"geoTiffPath": "/home/david/Downloads/glc2000_clipped.tif", "arrayName": "glc_2000_clipped", "attribute": "value", "outDirectory": "/home/david/scidb_data/0", }
-                    "glc": {"geoTiffPath": "/home/david/Downloads/glc2000.tif", "arrayName": "glc_2000", "attribute": "value", "outDirectory": "/home/david/scidb_data/0", }
+                    "glc": {"geoTiffPath": "/home/david/Downloads/glc2000_clipped.tif", "arrayName": "glc_2000_clipped", "attribute": "value", "outDirectory": "/home/david/scidb_data/0", }
+                    #"glc": {"geoTiffPath": "/home/david/Downloads/glc2000.tif", "arrayName": "glc_2000", "attribute": "value", "outDirectory": "/home/david/scidb_data/0", }
                     # "glc": {"geoTiffPath": "/home/04489/dhaynes/glc2000_clipped.tif", "arrayName": "glc_2000_clipped", "attribute": "value", "outDirectory": "/storage"}, 
                     #"meris": {"geoTiffPath": "/home/04489/dhaynes/meris_2010_clipped.tif", "arrayName": "meris_2010_clipped", "attribute": "value", "outDirectory": "/storage"}
                     #"nlcd": {"geoTiffPath": "/home/04489/dhaynes/nlcd_2006.tif", "arrayName": "nlcd_junk",  "attribute": "value", "outDirectory": "/storage"}
@@ -15,13 +15,20 @@ if __name__ == '__main__':
         sdb = iquery()
         for d in datasets:
                 #print(datasets[d])
-                raster = RasterLoader(datasets[d]["geoTiffPath"], datasets[d]["arrayName"], [datasets[d]["attribute"]], 0, datasets[d]["outDirectory"])
-                for r in raster.RasterMetadata:
-                    print("RasterMetadata")
-                    print(raster.RasterMetadata[r])
+                # raster = RasterLoader(datasets[d]["geoTiffPath"], datasets[d]["arrayName"], [datasets[d]["attribute"]], 0, datasets[d]["outDirectory"])
+                raster = RasterLoader(datasets[d]["geoTiffPath"], datasets[d]["arrayName"], [datasets[d]["attribute"]], 1000, datasets[d]["outDirectory"], 1, 2000000)
+                # RasterPath, scidbArray, attribute, chunksize, dataStorePath, tiles=None, maxPixels=10000000, yOffSet=None
+                # print("RasterMetadata")
+                # for r in raster.RasterMetadata:
+                #     print(raster.RasterMetadata[r])
+                # print("New Chunk Metadata")
+                # for r in raster.RasterReadingData:
+                #     print(raster.RasterReadingData[r])
                 #rasterArrayName, height, width, chunk
                 raster.CreateDestinationArray(datasets[d]["arrayName"], raster.height, raster.width, tileSizes[0])
-                ParallelLoad(raster.RasterMetadata)
+                # ParallelLoad(raster.RasterMetadata)
+                ParallelLoadByChunk(raster.RasterReadingData)
+
                 #loadAttribute = "%s_1:%s" % (raster.AttributeString.split(":")[0], raster.AttributeString.split(":")[1])
                 #print(loadAttribute)
                 #raster.CreateLoadArray("LoadArray", loadAttribute, raster.RasterArrayShape)
